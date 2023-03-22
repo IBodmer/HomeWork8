9a709c5783c992da9dfec4563690389af09e6269
IBodmer <pewpew308@gmail.com>
2023-03-22 10:58:57 +0300

    переделал чтобы был слой dao

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,59 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> root = cq.from(User.class);
+        cq.select(root);
+        TypedQuery<User> query = entityManager.createQuery(cq);
+        return query.getResultList();
+    }
+
+    @Transactional
+    @Override
+    public void update(Long id, User user) {
+        User user1 = entityManager.find(User.class, id);
+        user1.setId(user.getId());
+        user1.setName(user.getName());
+        user1.setLast_name(user.getLast_name());
+        user1.setEmail(user.getEmail());
+        user1.setAge(user.getAge());
+        entityManager.merge(user1);
+    }
+
+    @Transactional
+    @Override
+    public void delete(User user) {
+        entityManager.remove(user);
+    }
+
+}
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,57 +1,43 @@
 package web.service;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import org.springframework.stereotype.Service;
 import web.model.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
-@Repository
-@Transactional
-public class UserServiceImpl implements UserService {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
 
     @Override
     public void save(User user) {
-        entityManager.persist(user);
+        userDao.save(user);
     }
 
     @Override
     public User findById(Long id) {
-        return entityManager.find(User.class, id);
+        return userDao.findById(id);
     }
 
     @Override
     public List<User> findAll() {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<User> cq = cb.createQuery(User.class);
-        Root<User> root = cq.from(User.class);
-        cq.select(root);
-        TypedQuery<User> query = entityManager.createQuery(cq);
-        return query.getResultList();
+        return userDao.findAll();
     }
 
     @Override
     public void update(Long id, User user) {
-        User user1 = entityManager.find(User.class, id);
-        user1.setId(user.getId());
-        user1.setName(user.getName());
-        user1.setLast_name(user.getLast_name());
-        user1.setEmail(user.getEmail());
-        user1.setAge(user.getAge());
-        entityManager.merge(user1);
+        userDao.update(id, user);
     }
 
     @Override
     public void delete(User user) {
-        entityManager.remove(user);
+        userDao.delete(user);
     }
 }
